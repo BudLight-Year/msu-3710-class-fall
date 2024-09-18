cbfe48ac5deb9592e5f3a9c979609d2875c9c156
BudLight-Year <totallysweethobo@gmail.com>
2024-09-18 14:15:10 -0600

    US 1, 2, and 3 completed.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,9 @@
 class Student < ApplicationRecord
+    has_one_attached :profile_picture
+    MSUD_EMAIL_REGEX = /\A[\w+\-.]+@msudenver\.edu\z/i
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :major, presence: true
+    validates :school_email, presence: true, uniqueness: true, format: {with: MSUD_EMAIL_REGEX, message: "not a valid msu email address"}
+    validates :graduation_date, presence: true
 end
