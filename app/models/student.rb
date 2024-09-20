7c044f668ec771bc5f34a3f5f953b354b4a22248
BudLight-Year <totallysweethobo@gmail.com>
2024-09-20 10:46:27 -0600

    made profile images display in a reduced size

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,14 @@
 class Student < ApplicationRecord
-    has_one_attached :profile_picture
     MSUD_EMAIL_REGEX = /\A[\w+\-.]+@msudenver\.edu\z/i
+    
+    has_one_attached :profile_picture
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :major, presence: true
     validates :school_email, presence: true, uniqueness: true, format: {with: MSUD_EMAIL_REGEX, message: "not a valid msu email address"}
     validates :graduation_date, presence: true
+
+    def display_image
+        profile_picture.variant(resize_to_limit: [300, 300]).processed
+    end
 end
