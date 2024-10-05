4e2f2e3a37ab0e66119124f94faa2b6e658bd1ea
BudLight-Year <totallysweethobo@gmail.com>
2024-10-05 14:43:33 -0600

    added bootstrap, woking on majors search and form list integration

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,12 @@
 class Student < ApplicationRecord
     MSUD_EMAIL_REGEX = /\A[\w+\-.]+@msudenver\.edu\z/i
-    
+    VALID_MAJORS = ["Computer Science BS", "Cybersecurity Major", "Computer Information Systems BS", "Data Science and Machine Learning Major"]
+
+
     has_one_attached :profile_picture
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :major, presence: true
+    validates :major, presence: true, inclusion {in VALID_MAJORS, message: "%{value} is not a vaild major"}
     validates :school_email, presence: true, uniqueness: true, format: {with: MSUD_EMAIL_REGEX, message: "not a valid msu email address"}
     validates :graduation_date, presence: true
 
