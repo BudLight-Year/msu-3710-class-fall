18ec3b5d2cdb06e8e379f0967971738b5395ac2a
BudLight-Year <totallysweethobo@gmail.com>
2024-10-11 12:26:11 -0600

    Bootstrap is actually working with theme now. needs assets to precompile before use for some reason even in development

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "bootstrap", to: "bootstrap-8262dfcf8572fa786df01c8ee58dfc68ad4d40f0184b82ea564eac050a7e1f08.js"
